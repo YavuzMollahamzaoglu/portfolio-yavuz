43a5d35ed4f4085ab63a1eaeea1d1565ac6310b7
Yavuz Mollahamzaoglu <yavuzmollahamzaoglu@gmail.com>
2024-11-24 21:04:01 +0300

    updates 1.0.2

--- src/App.js
+++ src/App.js
@@ -752,7 +752,7 @@ function App() {
                       type="text"
                       name="user_name"
                       id="user_name"
-                      placeholder="Elon Musk"
+                      placeholder="Alan Turing"
                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                       required
                     />
@@ -768,7 +768,7 @@ function App() {
                     <input
                       type="email"
                       name="user_email"
-                      placeholder="örnek@gmail.com"
+                      placeholder="alanturingk@gmail.com"
                       id="user_email"
                       class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                       required
@@ -787,7 +787,7 @@ function App() {
                       name="subject"
                       id="subject"
                       class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                      placeholder="Konunuz"
+                      placeholder=""
                       required
                     />
                   </div>
@@ -804,7 +804,7 @@ function App() {
                       name="message"
                       rows="6"
                       class="block p-2.5 w-full text-sm text-gray-300 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                      placeholder="Mesajınızı buraya yazınız..."
+                      placeholder="Enter your message"
                       required
                     ></textarea>
                   </div>
